a7d3737a8ed22e2269731cc35373300efe524021
HELDER <hmedeiros211@gmail.com>
2023-03-16 18:23:36 +0100

    feat(html-css): :sparkles: basic html and css structure of the main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="nav-bar">HEADER</div>
+      <div className="cards">
+        <div className="card">
+          <input type="checkbox" />
+          <span>Titulo da Tarefa</span>
+        </div>
+        <div className="card">
+          <input type="checkbox" />
+          <span>Titulo da Tarefa</span>
+        </div>
+      </div>
+      <div className="fast-add-bar">
+        <input type="text" placeholder="Type the task here" />
+        <i className="bi bi-plus-circle-fill" />
+      </div>
+    </main>
   );
 }
 
