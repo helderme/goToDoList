822f12245f6ed7949ef59926dd1d865bee13d0f3
HELDER <hmedeiros211@gmail.com>
2023-03-17 10:54:20 +0100

    feat(component): :sparkles: function to remove and finish tasks

--- src/components/TasksCards.js
+++ src/components/TasksCards.js
@@ -4,19 +4,30 @@ import AppContext from '../context/AppContext';
 function TasksCards() {
   const {
     tasksList,
+    setTasksList,
   } = useContext(AppContext);
 
-  /*   const removeTask = (id) => {
+  const removeTask = (id) => {
+    const newList = tasksList.filter((task) => task.id !== id);
+    setTasksList(newList);
+  };
 
-  } */
+  const finishTask = (id) => {
+    const taskToFinish = tasksList.find((task) => task.id === id);
+    taskToFinish.status = 'done';
+    const updateTasks = tasksList.map((task) => (task.id === id ? taskToFinish : task));
+    setTasksList(updateTasks);
+  };
 
   return (
     <div className="cards">
       {tasksList.map((task) => (
-        <div className="card">
-          <input type="checkbox" />
+        <div className="card" key={task.id}>
+          <button type="button" onClick={() => finishTask(task.id)}>
+            <i className="bi bi-check-circle" />
+          </button>
           <span>{task.description}</span>
-          <button type="button">
+          <button type="button" onClick={() => removeTask(task.id)}>
             <i className="bi bi-trash3-fill" />
           </button>
         </div>
